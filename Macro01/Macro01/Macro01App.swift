4d99b12f5047d155d13d0901f27f3f0fceb86ee0
SarahSilva0 <sarahfrontdev@gmail.com>
2023-05-23 14:38:26 -0300

    Criando Projeto

---
+++ Macro01/Macro01/Macro01App.swift
@@ -0,0 +1,17 @@
+//
+//  Macro01App.swift
+//  Macro01
+//
+//  Created by Sarah dos Santos Silva on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Macro01App: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Macro01/Macro01/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Macro01
+//
+//  Created by Sarah dos Santos Silva on 23/05/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
