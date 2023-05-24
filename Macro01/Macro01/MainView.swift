24ebfa55b557b37c4dd298916a3370cfcaf9f1a3
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-05-24 17:04:00 -0300

    Botão Genérico Padrão Terminado

--- Macro01/Macro01/MainView.swift
+++ Macro01/Macro01/MainView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct MainView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            GeometryReader { geometry in
+               
+            }
         }
         .padding()
     }
