5ea5ba7ea52ab733e0e228b609c471c10cd66f4a
SarahSilva0 <91162156+SarahSilva0@users.noreply.github.com>
2023-06-05 16:39:53 -0300

    limpando pastas vazias

--- Macro01/Macro01/View/ CardOverlayView.swift
+++ Macro01/Macro01/View/ CardOverlayView.swift
@@ -1,32 +1,32 @@
-import SwiftUI
-
-struct CardOverlayView: View {
-    var cards: [CardModel]
-    var onSelectCard: ((CardModel) -> Void)
-    
-    var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                Color(.black)
-                    .opacity(0.7)
-                
-                VStack {
-                    Text("Cartas do Primeiro Jogador")
-                        .font(.headline)
-                    
-                    HStack(spacing: 10) {
-                        ForEach(cards) { card in
-                            CardComponent(image: Image(card.image))
-                                .onTapGesture {
-                                    onSelectCard(card)
-                                }
-                        }
-                        .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.2)
-                    }
-                }
-                .frame(width: geometry.size.width * 0.9)
-            }
-            .ignoresSafeArea(.all)
-        }
-    }
-}
+//import SwiftUI
+//
+//struct CardOverlayView: View {
+//    var cards: [CardModel]
+//    var onSelectCard: ((CardModel) -> Void)
+//    
+//    var body: some View {
+//        GeometryReader { geometry in
+//            ZStack {
+//                Color(.black)
+//                    .opacity(0.7)
+//                
+//                VStack {
+//                    Text("Cartas do Primeiro Jogador")
+//                        .font(.headline)
+//                    
+//                    HStack(spacing: 10) {
+//                        ForEach(cards) { card in
+//                            CardComponent(image: Image(card.image))
+//                                .onTapGesture {
+//                                    onSelectCard(card)
+//                                }
+//                        }
+//                        .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.2)
+//                    }
+//                }
+//                .frame(width: geometry.size.width * 0.9)
+//            }
+//            .ignoresSafeArea(.all)
+//        }
+//    }
+//}
