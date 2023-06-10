4fe9a94d3fc780af8c03637127204bbd9836b8f8
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-06-09 22:25:06 -0300

    Posicionado da melhor forma
    Irei arrumar melhor quando colocar o Geo

--- Macro01/Macro01/View/SheetView.swift
+++ Macro01/Macro01/View/SheetView.swift
@@ -17,67 +17,71 @@ struct SheetView: View {
     
     //Controlar o estado do timer
     @State private var timer: Timer?
-   
+    
     
     var body: some View {
-        HStack {
-            ForEach(combatViewModel.player1.cards, id: \.self) { card in
-                CardComponent(image: Image(card))
-                    .onTapGesture {
-                        combatViewModel.player1.selectedCard = card
-                        isSheetVisible = false
-                        combatViewModel.selectedCardPlayer2()
-                        combatViewModel.endTurn()
-                        closeSheet()
-                        
-                    }
+        
+        ZStack {
+            
+            HStack {
+                ForEach(combatViewModel.player1.cards, id: \.self) { card in
+                    CardComponent(image: Image(card))
+                        .onTapGesture {
+                            combatViewModel.player1.selectedCard = card
+                            isSheetVisible = false
+                            combatViewModel.selectedCardPlayer2()
+                            combatViewModel.endTurn()
+                            closeSheet()
+                            
+                        }
+                }
+                
             }
             
             VStack {
-                Spacer()
-                
                 ZStack(alignment: .bottom) {
                     RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 20, height: 100)
+                        .frame(width: 15, height: 230)
                         .foregroundColor(Color(hex: "FFF2D9"))
                     
                     RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 20, height: 100 * countdownFraction())
+                        .frame(width: 15, height: 230 * countdownFraction())
                         .foregroundColor(Color(hex: "688869"))
-                
+                    
                 }
                 
-                Spacer()
             }
-        }
-        .onAppear {
-            startCountdownSheet()
+            .padding(.leading, 480)
+           
+            .onAppear {
+                startCountdownSheet()
+            }
         }
     }
     
     
     func startCountdownSheet() {
-            if timer != nil {
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if countdownSheet > 0 {
+                countdownSheet -= 1
+            } else {
                 timer?.invalidate()
-                timer = nil
-            }
-            
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                if countdownSheet > 0 {
-                    countdownSheet -= 1
-                } else {
-                    timer?.invalidate()
-//                    timer = nil
-                    
-                    self.countdownSheet = 5
-                    self.closeSheet()
-                    //Falar que jogador 1 perdeu
-                    combatViewModel.selectedCardPlayer2()
-                    combatViewModel.endTurn()
-                }
+                
+                
+                self.countdownSheet = 5
+                self.closeSheet()
+                //Falar que jogador 1 perdeu
+                combatViewModel.selectedCardPlayer2()
+                combatViewModel.endTurn()
             }
         }
-       
+    }
+    
     
     func countdownFraction() -> CGFloat {
         let totalDuration: CGFloat = 5
@@ -91,7 +95,7 @@ struct SheetView: View {
         countdownSheet = 5
         isSheetVisible = false
     }
-
     
-  
+    
+    
 }
