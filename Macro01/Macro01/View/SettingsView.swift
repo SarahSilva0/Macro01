6ba710958ccc52cafabd59367aa0bf1801152016
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-06-14 10:51:20 -0300

    Geometry da tela

---
+++ Macro01/Macro01/View/SettingsView.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsView.swift
+//  Macro01
+//
+//  Created by Higor  Lo Castro on 14/06/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+    var body: some View {
+        GeometryReader { geometry in
+            
+            VStack {
+                
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+        }
+    }
+}
+
+
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
