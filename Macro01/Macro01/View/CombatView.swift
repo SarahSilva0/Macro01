428f561109051958737b7ef0c6b8b575fad38b2d
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-06-14 22:36:33 -0300

    Fundo e Contador ao centro

--- Macro01/Macro01/View/CombatView.swift
+++ Macro01/Macro01/View/CombatView.swift
@@ -15,147 +15,47 @@ struct CombatView: View {
     
     
     var body: some View {
+        
         GeometryReader { geometry in
-            VStack {
-                ZStack {
-                    Image("background")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.6)
-                        .padding(.top, -20)
-                    
-                    HStack (alignment: .center) {
-                        if combatViewModel.isCountdownVisible {
+            ZStack {
+                Image("backgroundOne")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .ignoresSafeArea()
+                
+        
+                VStack(alignment: .center) {
+                    Spacer ()
+                    if combatViewModel.isCountdownVisible {
                             Text("\(combatViewModel.countdown)")
                                 .font(.custom("Helvetica Neue", size: 50))
-                                .foregroundColor(Color (hex: "3C3634"))
+                                .foregroundColor(Color(hex: "3C3634"))
                                 .fontWeight(.bold)
                         }
+                    Spacer()
                     }
                     .frame(width: geometry.size.width * 0.1, height: geometry.size.height * 0.1)
-                    //                                    .background(Color.white)
-                    
-                    VStack {
-                        HStack (alignment: .center) {
-                            //Placar
-                            ScoreView(player1Wins: combatViewModel.player1.winTurno,
-                                      player2Wins: combatViewModel.player2.winTurno)
-                            
-                        }
-                        .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.2)
-                        //                                        .background(Color.yellow)
-                        
-                        Spacer (minLength: 100)
-                        
-                        VStack {
-                            VStack (alignment: .center){
-                                HStack (spacing: 100) {
-                                    //CARTA AO CENTRO DO PLAYER 1
-                                    VStack {
-                                        Image(combatViewModel.player1.selectedCard)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fill)
-                                    }
-                                    .frame(width: geometry.size.width * 0.13, height: geometry.size.height * 0.13)
-                                    
-                                    //CARTA AO CENTRO DO PLAYER 1
-                                    VStack {
-                                        //Mudei para o player2 Receber a carta quando a Sheet Fechar
-                                        if !combatViewModel.isSheetVisible {
-                                            Image(combatViewModel.player2.selectedCard)
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                        }
-                                        else{
-                                            Image("")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fill)
-                                        }
-                                    }
-                                    .frame(width: geometry.size.width * 0.13, height: geometry.size.height * 0.13)
-                                }
-                                .frame(width: geometry.size.width * 0.5, height: geometry.size.height * 0.3)
-                                //                                .background(.blue)
-                            }
-                        }
-                        
-                        Spacer(minLength: 100)
-                        
-                        //CARTAS DO PLAYER 1
-                        HStack {
-                            ForEach(combatViewModel.player1.cards, id: \.self) { card in
-                                CardComponent(image: Image(card))
-                            }
-                        }
-                        .allowsHitTesting(combatViewModel.isInteractionEnabled)
-                        //                    .background(.white)
-                        .frame(width: geometry.size.width * 0.2, height: geometry.size.height * 0.2)
-                    }
-                    
-                    HStack {
-                        ZStack {
-                            VStack {
-                                Spacer()
-                                CharacterCircle(sizeWidth: 180, sizeHeight: 180, colorHex: "FFC097")
-                            }
-                            .padding(.bottom, 50) // Mover o círculo para cima
-                            
-                            VStack {
-                                Spacer(minLength: 420)
-                                Character(character: "player1")
-                            }
-                            .frame(width: geometry.size.width * 0.30, height: geometry.size.height * 0.30)
-                        }
-                        
-                        Spacer(minLength: 100)
-                        
-                        ZStack {
-                            VStack {
-                                Spacer()
-                                CharacterCircle(sizeWidth: 180, sizeHeight: 180, colorHex: "89B9E5")
-                            }
-                            .padding(.bottom, 50) // Mover o círculo para cima
-                            
-                            VStack {
-                                Spacer(minLength: 420)
-                                Character(character: "player1")
-                            }
-                            .frame(width: geometry.size.width * 0.30, height: geometry.size.height * 0.30)
-                        }
-                    }
-                    HStack{
-                        ButtonGenRound(action: {
-                            print("Galeria")
-                        },
-                                       image: "pause",
-                                       foregroundColor: Color(hex: "FFF2D9"),
-                                       backgroundColor: Color(hex: "FFF2D9"))
-                        .frame(width: 70, height: 70)
-                        Spacer()
-                    }
-                    .frame(width: geometry.size.width, height: geometry.size.height * 1.1, alignment: .topLeading)
-//                    .background(.yellow)
+                
+                
+                .onAppear {
+                    combatViewModel.startCountdown()
                 }
-            }
-            .background (Color(hex: "3C3634"))
-            .ignoresSafeArea()
-            
-            .onAppear {
-                combatViewModel.startCountdown()
-            }
-            
-            .sheet(isPresented: $combatViewModel.isSheetVisible, onDismiss: {
-            }) {
-                SheetView(combatViewModel: combatViewModel, isSheetVisible: $combatViewModel.isSheetVisible, countdownSheet: $combatViewModel.countdownSheet).background(ClearBackgroundView())
-            }
-            .alert(isPresented: $combatViewModel.isGameEndAlertPresented) {
-                Alert(title: Text("Fim do Jogo"),
-                      message: Text("\(combatViewModel.getScore())"),
-                      dismissButton: .default(Text("OK"), action: {
-                    presentationMode.wrappedValue.dismiss()
-                }))
+                
+                .sheet(isPresented: $combatViewModel.isSheetVisible, onDismiss: {
+                }) {
+                    SheetView(combatViewModel: combatViewModel, isSheetVisible: $combatViewModel.isSheetVisible, countdownSheet: $combatViewModel.countdownSheet).background(ClearBackgroundView())
+                }
+                .alert(isPresented: $combatViewModel.isGameEndAlertPresented) {
+                    Alert(title: Text("Fim do Jogo"),
+                          message: Text("\(combatViewModel.getScore())"),
+                          dismissButton: .default(Text("OK"), action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }))
+                }
+                
             }
         }
+   
     }
 }
 
