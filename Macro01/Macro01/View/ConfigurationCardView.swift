092c76b5bd6572c1b01ab66567127c42838ba856
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-03 18:03:57 -0300

    Centralizado

--- Macro01/Macro01/View/ConfigurationCardView.swift
+++ Macro01/Macro01/View/ConfigurationCardView.swift
@@ -4,51 +4,54 @@ struct ConfigurationCardView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            VStack {
-                HStack {
-                    Text("Configuração")
-                        .font(.system(size: 20))
-                        .fontWeight(.bold)
-                    
-                    Button(action: {
-                        // Ação a ser executada
-                    }) {
-                        HStack (alignment: .center){
-                            ZStack {
-                                Circle()
-                                    .foregroundColor(.black)
-                                    .frame(width: 44, height: 44)
-                                
-                                Image(systemName: "speaker.wave.2.fill")
-                                    .font(.system(size: 20))
-                                    .foregroundColor(Color(hex: "FFF2D9"))
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Configuração")
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                        
+                        Button(action: {
+                            // Ação a ser executada
+                        }) {
+                            HStack {
+                                ZStack {
+                                    Circle()
+                                        .foregroundColor(.black)
+                                        .frame(width: 44, height: 44)
+                                    
+                                    Image(systemName: "speaker.wave.2.fill")
+                                        .font(.system(size: 20))
+                                        .foregroundColor(Color(hex: "FFF2D9"))
+                                }
                             }
                         }
                     }
+                    
+                    VStack {
+                        TransparentConfigurationButton(text: "Tutorial", icon: "book") {
+                            print("Tutorial")
+                        }
+                        TransparentConfigurationButton(text: "Sobre", icon: "info") {
+                            print("Sobre")
+                        }
+                        TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info") {
+                            print("Privacidade")
+                        }
+                        TransparentConfigurationButton(text: "Suporte", icon: "email") {
+                            print("email")
+                        }
+                    }
+                    
                 }
-                
-                VStack {
-                    TransparentConfigurationButton(text: "Tutorial", icon: "book") {
-                        print("Tutorial")
-                    }
-                    TransparentConfigurationButton(text: "Sobre", icon: "info") {
-                        print("Sobre")
-                    }
-                    TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info") {
-                        print("Privacidade")
-                    }
-                    TransparentConfigurationButton(text: "Suporte", icon: "email") {
-                        print("email")
-                    }
-                }
+                .frame(width: geometry.size.width * 0.4, height: geometry.size.height)
+                .background(Color(hex: "FFF2D9"))
+                .cornerRadius(10)
+                .shadow(radius: 2)
                 
             }
             .frame(width: geometry.size.width, height: geometry.size.height)
         }
-        .frame(width: 300, height: 390)
-        .background(Color(hex: "FFF2D9"))
-        .cornerRadius(10)
-        .shadow(radius: 2)
     }
 }
 
