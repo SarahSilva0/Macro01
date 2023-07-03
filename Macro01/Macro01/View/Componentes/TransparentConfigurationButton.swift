b8d6a1f91f7835fc4b79f1966822e20c29ac1402
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-03 17:35:30 -0300

    Falta ajustes

--- Macro01/Macro01/View/Componentes/TransparentConfigurationButton.swift
+++ Macro01/Macro01/View/Componentes/TransparentConfigurationButton.swift
@@ -10,31 +10,35 @@ import SwiftUI
 struct TransparentConfigurationButton: View {
     let text: String
     let icon: String
+    let action: () -> Void
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 5)
-                .frame(width: 280, height: 35)
-                .foregroundColor(Color.gray.opacity(0.2))
-            
-            HStack {
-                Image(icon)
-                    .resizable()
-                    .aspectRatio( contentMode: .fit)
-                    .frame(width: 25,height: 25)
+        Button(action: action) {
+        
+            ZStack {
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: 280, height: 35)
+                    .foregroundColor(Color.gray.opacity(0.2))
+                
+                HStack {
+                    Image(icon)
+                        .resizable()
+                        .aspectRatio( contentMode: .fit)
+                        .frame(width: 25,height: 25)
                     
-                    
-                Text(text)
-                    .font(.system(size: 15))
-                    .foregroundColor(.black)
+                    Text(text)
+                        .font(.system(size: 15))
+                        .foregroundColor(.black)
+                }
             }
         }
-        
     }
 }
 
 struct TransparentConfigurationButton_Previews: PreviewProvider {
     static var previews: some View {
-        TransparentConfigurationButton(text: "Tutorial", icon: "email")
+        TransparentConfigurationButton(text: "Suporte", icon: "email") {
+            print("email")
+        }
     }
 }
--- Macro01/Macro01/View/ConfigurationCardView.swift
+++ Macro01/Macro01/View/ConfigurationCardView.swift
@@ -1,41 +1,49 @@
 import SwiftUI
 
 struct ConfigurationCardView: View {
-
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 HStack {
-                        Text("Configuração")
-                            .font(.system(size: 20))
-                            .fontWeight(.bold)
-                        
-                        Button(action: {
-                            // Ação a ser executada
-                        }) {
-                            HStack (alignment: .center){
-                                ZStack {
-                                    Circle()
-                                        .foregroundColor(.black)
-                                        .frame(width: 44, height: 44)
-                                    
-                                    Image(systemName: "speaker.wave.2.fill")
-                                        .font(.system(size: 20))
-                                        .foregroundColor(Color(hex: "FFF2D9"))
-                                }
+                    Text("Configuração")
+                        .font(.system(size: 20))
+                        .fontWeight(.bold)
+                    
+                    Button(action: {
+                        // Ação a ser executada
+                    }) {
+                        HStack (alignment: .center){
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(.black)
+                                    .frame(width: 44, height: 44)
+                                
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .font(.system(size: 20))
+                                    .foregroundColor(Color(hex: "FFF2D9"))
                             }
                         }
                     }
-                    .background(.red)
-                VStack {
-                    TransparentConfigurationButton(text: "Tutorial", icon: "book")
-                    TransparentConfigurationButton(text: "Sobre", icon: "info")
-                    TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info")
-                    TransparentConfigurationButton(text: "Suporte", icon: "email")
                 }
-                    
                 
-            } .frame(width: geometry.size.width, height: geometry.size.height)
+                VStack {
+                    TransparentConfigurationButton(text: "Tutorial", icon: "book") {
+                        print("Tutorial")
+                    }
+                    TransparentConfigurationButton(text: "Sobre", icon: "info") {
+                        print("Sobre")
+                    }
+                    TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info") {
+                        print("Privacidade")
+                    }
+                    TransparentConfigurationButton(text: "Suporte", icon: "email") {
+                        print("email")
+                    }
+                }
+                
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
         }
         .frame(width: 300, height: 390)
         .background(Color(hex: "FFF2D9"))
