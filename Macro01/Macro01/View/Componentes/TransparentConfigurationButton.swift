516377c0a8eda26f6ea89005b77eda90eade43d9
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-04 16:11:30 -0300

    Falta apenas o botao

--- Macro01/Macro01/View/Componentes/TransparentConfigurationButton.swift
+++ Macro01/Macro01/View/Componentes/TransparentConfigurationButton.swift
@@ -13,27 +13,40 @@ struct TransparentConfigurationButton: View {
     let action: () -> Void
     
     var body: some View {
-        Button(action: action) {
         
-            ZStack {
-                RoundedRectangle(cornerRadius: 5)
-                    .frame(width: 280, height: 35)
-                    .foregroundColor(Color.gray.opacity(0.2))
-                
-                HStack {
-                    Image(icon)
-                        .resizable()
-                        .aspectRatio( contentMode: .fit)
-                        .frame(width: 25,height: 25)
+        GeometryReader { geometry in
+            Button(action: action) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .frame(width: 280, height: 35)
+                        .foregroundColor(Color.gray.opacity(0.2))
                     
-                    Text(text)
-                        .font(.system(size: 15))
-                        .foregroundColor(.black)
+                    HStack {
+                        Image(icon)
+                            .resizable()
+                            .aspectRatio( contentMode: .fit)
+                            .frame(width: 25,height: 25)
+                        Spacer()
+                    }
+                    .frame(width: geometry.size.width * 0.9)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text(text)
+                            .font(.system(size: 15))
+                            .foregroundColor(.black)
+                    }
+
                 }
+                .frame(width: geometry.size.width, height: geometry.size.height)
             }
         }
+        
     }
 }
+
+
 
 struct TransparentConfigurationButton_Previews: PreviewProvider {
     static var previews: some View {
