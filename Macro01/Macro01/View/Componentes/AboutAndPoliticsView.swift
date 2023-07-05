b28fb41b1c141d2407d67e4bb1d16c77b4692d0f
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-05 20:00:51 -0300

    View

--- Macro01/Macro01/View/Componentes/AboutAndPoliticsView.swift
+++ Macro01/Macro01/View/Componentes/AboutAndPoliticsView.swift
@@ -7,14 +7,101 @@
 
 import SwiftUI
 
-struct AboutAndPoliticsView: View {
+import SwiftUI
+
+struct AboutAndPolitics: View {
+    @ScaledMetric(relativeTo: .body) var buttonSize: CGFloat = 50
+    @Binding var isPresented: Bool
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                Color(.black)
+                    .opacity(0.65)
+                
+                VStack {
+                    HStack {
+                        ButtonGenRound(action: {
+                            withAnimation {
+                                isPresented = false
+                            }
+                            
+                        },
+                        image: "out",
+                        foregroundColor: (Color(hex: "FFF2D9")),
+                        backgroundColor: (Color(hex: "FFF2D9")))
+                        .frame(width: buttonSize, height: buttonSize)
+                      
+                        Spacer()
+                        
+                    }
+                    Spacer()
+                        .frame(width: geometry.size.width * 0.01, height: geometry.size.height * 0.69)
+                        .background(.white)
+                    
+                }
+                .frame(width: geometry.size.width * 0.86, height: geometry.size.height * 0.1)
+                
+                HStack {
+                    VStack  {
+                        ZStack {
+                            HStack  {
+                                Text("Configuração")
+                                    .font(.system(size: 20))
+                                    .fontWeight(.bold)
+                            }
+                            
+                            HStack  {
+                                Spacer()
+                                ButtonGenRound(action: {
+                                    print("SOM")
+                                },
+                                               image: "sound",
+                                               foregroundColor: (Color(hex: "FFF2D9")),
+                                               backgroundColor: (Color(hex: "FFF2D9")))
+                            }
+                            .frame(width: geometry.size.width * 0.38, height: geometry.size.height * 0.2)
+                        }
+                        
+                        Spacer()
+                            .frame(height: geometry.size.height * 0.1)
+                        
+//                        VStack {
+//                            TransparentConfigurationButton(text: "Tutorial", icon: "book") {
+//                                print("Tutorial")
+//                            }
+//                            TransparentConfigurationButton(text: "Sobre", icon: "info") {
+//                                print("Sobre")
+//                            }
+//                            TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info") {
+//                                print("Privacidade")
+//                            }
+//                            TransparentConfigurationButton(text: "Suporte", icon: "email") {
+//                                print("email")
+//                            }
+//                        }
+                        .frame(width: geometry.size.width * 0.4, height: geometry.size.height * 0.5)
+                        Spacer()
+                    }
+                    .frame(width: geometry.size.width * 0.4)
+                    .background(Color(hex: "FFF2D9"))
+                    .cornerRadius(10)
+                    .shadow(radius: 2)
+                }
+            }
+            .ignoresSafeArea()
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .transition(.move(edge: .leading))
+            .animation(.easeOut(duration: 4), value: false)
+            
+            .onDisappear {
+                DispatchQueue.main.async {
+                    isPresented = false
+                }
+            }
+        }
     }
 }
 
-struct AboutAndPoliticsView_Previews: PreviewProvider {
-    static var previews: some View {
-        AboutAndPoliticsView()
-    }
-}
+
