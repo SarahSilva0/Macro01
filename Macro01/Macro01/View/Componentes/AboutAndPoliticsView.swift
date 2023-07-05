82569d0f91633d24cb4ca9c3995ed73e82ca91f6
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-05 19:58:56 -0300

    VIew Criada

---
+++ Macro01/Macro01/View/Componentes/AboutAndPoliticsView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAndPoliticsView.swift
+//  Macro01
+//
+//  Created by Higor  Lo Castro on 05/07/23.
+//
+
+import SwiftUI
+
+struct AboutAndPoliticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutAndPoliticsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutAndPoliticsView()
+    }
+}
