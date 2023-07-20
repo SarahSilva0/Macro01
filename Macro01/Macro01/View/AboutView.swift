c84181bd7c0627814e76aa0c816ce2913c4447c2
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-20 16:03:03 -0300

    Terminado

---
+++ Macro01/Macro01/View/AboutView.swift
@@ -0,0 +1,86 @@
+//
+//  AboutView.swift
+//  Macro01
+//
+//  Created by Higor  Lo Castro on 20/07/23.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    @ScaledMetric(relativeTo: .body) var buttonSize: CGFloat = 50
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color(.black)
+                    .opacity(0.65)
+                
+                VStack {
+                    HStack {
+                        ButtonGenRound(action: {
+                            withAnimation {
+                                isPresented = false
+                            }
+                            
+                        },
+                        image: "out",
+                        foregroundColor: (Color(hex: "FFF2D9")),
+                        backgroundColor: (Color(hex: "FFF2D9")))
+                        .frame(width: buttonSize, height: buttonSize)
+                      
+                        Spacer()
+                        
+                    }
+                    Spacer()
+                        .frame(width: geometry.size.width * 0.01, height: geometry.size.height * 0.69)
+                    
+                }
+                .frame(width: geometry.size.width * 0.86, height: geometry.size.height * 0.1)
+                Spacer()
+                
+                    VStack {
+                        HStack  {
+                            Text("Sobre")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                        }
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.15)
+
+                        HStack {
+                            
+                            Text("""
+                             Folkard é um jogo de cartas com tema
+                             folclórico, onde seu objetivo é duelar contra
+                             seus oponentes para conquistar todas as
+                             cartas!
+                             """
+                            )
+                            .font(Font.custom("SF Pro", size: 12))
+                            .multilineTextAlignment(.center)
+                            .lineSpacing(5)
+                        }
+                        .frame(width: geometry.size.width, height: geometry.size.height * 0.7)
+                        Spacer()
+                            .background(.red)
+                    }
+                    .frame(width: geometry.size.width * 0.4)
+                    .background(Color(hex: "FFF2D9"))
+                    .cornerRadius(10)
+                    .shadow(radius: 2)
+
+            }
+            .ignoresSafeArea()
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .transition(.move(edge: .leading))
+            .animation(.easeOut(duration: 4), value: false)
+            
+            .onDisappear {
+                DispatchQueue.main.async {
+                    isPresented = false
+                }
+            }
+        }
+    }
+}
---
+++ Macro01/Macro01/View/ConfigurationCardView.swift
@@ -0,0 +1,114 @@
+//
+//  ConfigurationCardView.swift
+//  Macro01
+//
+//  Created by Higor  Lo Castro on 20/07/23.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct ConfigurationCardView: View {
+    @ScaledMetric(relativeTo: .body) var buttonSize: CGFloat = 50
+   
+    @Binding var isPresented: Bool
+    @State private var showAbout = false
+    @State private var showPolicyAndPrivacy = false
+
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color(.black)
+                    .opacity(0.65)
+                    .onTapGesture {}
+                
+                VStack {
+                    HStack {
+                        ButtonGenRound(action: {
+                            withAnimation {
+                                isPresented = false
+                            }
+                        },
+                        image: "out",
+                        foregroundColor: (Color(hex: "FFF2D9")),
+                        backgroundColor: (Color(hex: "FFF2D9")))
+                        .frame(width: buttonSize, height: buttonSize)
+                      
+                        Spacer()
+                        
+                    }
+                    Spacer()
+                        .frame(width: geometry.size.width * 0.01, height: geometry.size.height * 0.69)
+                }
+                .frame(width: geometry.size.width * 0.86, height: geometry.size.height * 0.1)
+                
+                HStack {
+                    VStack  {
+                        ZStack {
+                            HStack  {
+                                Text("Configuração")
+                                    .font(.system(size: 20))
+                                    .fontWeight(.bold)
+                            }
+                            
+                            HStack  {
+                                Spacer()
+                                ButtonGenRound(action: {
+                                    print("SOM")
+                                },
+                                               image: "sound",
+                                               foregroundColor: (Color(hex: "FFF2D9")),
+                                               backgroundColor: (Color(hex: "FFF2D9")))
+                            }
+                            .frame(width: geometry.size.width * 0.38, height: geometry.size.height * 0.2)
+                        }
+                        
+                        Spacer()
+                            .frame(height: geometry.size.height * 0.1)
+                        
+                        VStack {
+                            TransparentConfigurationButton(text: "Tutorial", icon: "book") {
+                                print("Tutorial")
+                            }
+                            TransparentConfigurationButton(text: "Sobre", icon: "info") {
+                                showAbout = true
+                            }
+                            
+                            TransparentConfigurationButton(text: "Suporte", icon: "email") {
+                                print("email")
+                            }
+                            
+                            TransparentConfigurationButton(text: "Políticas de Privacidade", icon: "info") {
+                               showPolicyAndPrivacy = true
+                            }
+                        }
+                        .frame(width: geometry.size.width * 0.4, height: geometry.size.height * 0.5)
+                        Spacer()
+                    }
+                    .frame(width: geometry.size.width * 0.4)
+                    .background(Color(hex: "FFF2D9"))
+                    .cornerRadius(10)
+                    .shadow(radius: 2)
+                }
+            }
+            .ignoresSafeArea()
+            .frame(width: geometry.size.width, height: geometry.size.height)
+            .transition(.move(edge: .leading))
+            .animation(.easeOut(duration: 4), value: false)
+            
+            .sheet(isPresented: $showAbout) {
+                AboutView(isPresented: $showAbout).background(ClearBackgroundView())
+            }
+            .sheet(isPresented: $showPolicyAndPrivacy) {
+                PolicyAndPrivacyView(isPresented: $showPolicyAndPrivacy).background(ClearBackgroundView())
+            }
+            .onDisappear {
+                DispatchQueue.main.async {
+                    isPresented = false
+                }
+            }
+        }
+    }
+}
