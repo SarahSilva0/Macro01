e579ae72059faa1f72cdc99db02601a5bd051cc9
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-20 19:55:34 -0300

    Corrigindo View

--- Macro01/Macro01/View/FisrtView .swift
+++ Macro01/Macro01/View/FisrtView .swift
@@ -5,21 +5,15 @@
 //  Created by Sarah dos Santos Silva on 29/05/23.
 //
 
-//
-//  FisrtView .swift
-//  Macro01
-//
-//  Created by Sarah dos Santos Silva on 29/05/23.
-//
-
 import SwiftUI
 
 struct FirstView: View {
     @ScaledMetric(relativeTo: .body) var buttonSize: CGFloat = 50
+    @State private var showConfiguration = false
     
     var body: some View {
         GeometryReader { geometry in
-            ZStack{
+            ZStack {
                 Color.black
                     .ignoresSafeArea()
                 Image("BG GERAL")
@@ -27,57 +21,87 @@ struct FirstView: View {
                     .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea(.all)
                 VStack {
-                    VStack{
+                    VStack {
                         HStack(spacing: 20) {
                             ButtonGenRound(action: {
-                                print("Configuração")
+                                withAnimation {
+                                    showConfiguration = true
+                                }
                             },
-                                           image: "config",
-                                           foregroundColor: Color(hex: "FFF2D9"),
-                                           backgroundColor: Color(hex: "FFF2D9"))
+                            image: "config",
+                            foregroundColor: Color(hex: "FFF2D9"),
+                            backgroundColor: Color(hex: "FFF2D9"))
                             .frame(width: buttonSize, height: buttonSize)
                             
                             ButtonGenRound(action: {
                                 print("Galeria")
                             },
-                                           image: "Galeria",
-                                           foregroundColor: Color(hex: "FFF2D9"),
-                                           backgroundColor: Color(hex: "FFF2D9"))
+                            image: "Galeria",
+                            foregroundColor: Color(hex: "FFF2D9"),
+                            backgroundColor: Color(hex: "FFF2D9"))
                             .frame(width: buttonSize, height: buttonSize)
                             
                             Spacer()
                         }
                         .padding(.leading)
-                    }.frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
+                    }
+                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
                     
                     VStack {
-                       Image("titulo")
+                        Image("titulo")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .padding(.vertical)
-        
-                            NavigationLink(destination: DificultyView_(combatViewModel: CombatViewModel())){
-                                Text("Jogo Livre")
-                                    .font(.title3)
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.black)
-                                    .frame(width: 200, height: 15)
-                                    .padding()
-                                    .background(Color(hex: "FFF2D9"))
-                                    .cornerRadius(10)
-                            }
                         
-                    }.frame(width: geometry.size.width * 0.7, height: geometry.size.height * 0.8, alignment: .center)
+                        NavigationLink(destination: DificultyView_(combatViewModel: CombatViewModel())) {
+                            Text("Jogo Livre")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundColor(.black)
+                                .frame(width: 200, height: 15)
+                                .padding()
+                                .background(Color(hex: "FFF2D9"))
+                                .cornerRadius(10)
+                        }
+                        
+                    }
+                    .frame(width: geometry.size.width * 0.7, height: geometry.size.height * 0.8, alignment: .center)
+                    .overlay(
+                        Group {
+                            if showConfiguration {
+                                ConfigurationCardView(isPresented: $showConfiguration)
+                                    .background(ClearBackgroundView())
+                                    .transition(.move(edge: .leading))
+                                    .onTapGesture {
+                                        withAnimation {
+                                            showConfiguration = false
+                                        }
+                                    }
+                            }
+                        }
+                        .onAppear {
+                            withAnimation {
+                                showConfiguration = false
+                            }
+                        }
+                    )
                 }
-            }.frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
         }
     }
 }
-
-
 
 struct FisrtView__Previews: PreviewProvider {
     static var previews: some View {
         FirstView()
     }
 }
+
+
+
+//
+//}
+//    .navigationBarHidden(true)
+//}
+//}
