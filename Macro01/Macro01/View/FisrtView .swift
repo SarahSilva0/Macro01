7e1c0df6d4e0c92168f75b5935a254a110ee1767
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-20 21:38:19 -0300

    View Transparente tela toda

--- Macro01/Macro01/View/FisrtView .swift
+++ Macro01/Macro01/View/FisrtView .swift
@@ -12,88 +12,92 @@ struct FirstView: View {
     @State private var showConfiguration = false
     
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                Color.black
-                    .ignoresSafeArea()
-                Image("BG GERAL")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea(.all)
-                VStack {
+        NavigationView {
+            GeometryReader { geometry in
+                ZStack {
+                    Color.black
+                        .ignoresSafeArea()
+                    Image("BG GERAL")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .ignoresSafeArea(.all)
                     VStack {
-                        HStack(spacing: 20) {
-                            ButtonGenRound(action: {
-                                withAnimation {
-                                    showConfiguration = true
-                                }
-                            },
-                            image: "Configuração",
-                            foregroundColor: Color(hex: "FFF2D9"),
-                            backgroundColor: Color(hex: "FFF2D9"))
-                            .frame(width: buttonSize, height: buttonSize)
-                            
-                            ButtonGenRound(action: {
-                                print("Galeria")
-                            },
-                            image: "Galeria",
-                            foregroundColor: Color(hex: "FFF2D9"),
-                            backgroundColor: Color(hex: "FFF2D9"))
-                            .frame(width: buttonSize, height: buttonSize)
-                            
-                            Spacer()
-                        }
-                        .padding(.leading)
-                    }
-                    .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
-                    
-                    VStack {
-                        Image("titulo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .padding(.vertical)
-                        
-                        NavigationLink(destination: DificultyView_(combatViewModel: CombatViewModel())) {
-                            Text("Jogo Livre")
-                                .font(.title3)
-                                .fontWeight(.bold)
-                                .foregroundColor(.black)
-                                .frame(width: 200, height: 15)
-                                .padding()
-                                .background(Color(hex: "FFF2D9"))
-                                .cornerRadius(10)
-                        }
-                        
-                    }
-                    .frame(width: geometry.size.width * 0.7, height: geometry.size.height * 0.8, alignment: .center)
-                    .overlay(
-                        Group {
-                            if showConfiguration {
-                                ConfigurationCardView(isPresented: $showConfiguration)
-                                    .background(ClearBackgroundView())
-                                    .transition(.move(edge: .leading))
-                                    .onTapGesture {
-                                        withAnimation {
-                                            showConfiguration = false
+                        VStack {
+                            HStack(spacing: 20) {
+                                ButtonGenRound(action: {
+                                    withAnimation {
+                                        showConfiguration = true
                                     }
+                                },
+                                               image: "Configuração",
+                                               foregroundColor: Color(hex: "FFF2D9"),
+                                               backgroundColor: Color(hex: "FFF2D9"))
+                                .frame(width: buttonSize, height: buttonSize)
+                                
+                                ButtonGenRound(action: {
+                                    print("Galeria")
+                                },
+                                               image: "Galeria",
+                                               foregroundColor: Color(hex: "FFF2D9"),
+                                               backgroundColor: Color(hex: "FFF2D9"))
+                                .frame(width: buttonSize, height: buttonSize)
+                                
+                                Spacer()
+                            }
+                            .padding(.leading)
+                        }
+                        .frame(width: geometry.size.width * 0.9, height: geometry.size.height * 0.1)
+                        
+                        VStack {
+                            Image("titulo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .padding(.vertical)
+                            
+                            NavigationLink(destination: DificultyView_(combatViewModel: CombatViewModel())) {
+                                Text("Jogo Livre")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .frame(width: 200, height: 15)
+                                    .padding()
+                                    .background(Color(hex: "FFF2D9"))
+                                    .cornerRadius(10)
+                            }
+                        }
+                        .padding()
+                    }
+                }
+            
+            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+            .overlay(
+                Group {
+                    if showConfiguration {
+                        ConfigurationCardView(isPresented: $showConfiguration)
+                            .background(ClearBackgroundView())
+                            .transition(.move(edge: .leading))
+                            .onTapGesture {
+                                withAnimation {
+                                    showConfiguration = false
                                 }
                             }
-                        }
-                        .onAppear {
-                            withAnimation {
-                                showConfiguration = false
-                            }
-                        }
-                    )
+                    }
                 }
-                .navigationBarHidden(true)
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height, alignment: .center)
+                
+                    .onAppear {
+                        withAnimation {
+                            showConfiguration = false
+                        }
+                    }
+            )
         }
-
+        .navigationBarHidden(true)
     }
-        
+    }
 }
+
+
+
 
 struct FisrtView__Previews: PreviewProvider {
     static var previews: some View {
