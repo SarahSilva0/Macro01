c5971bc6e21b21d8a61cca457c57f132bd31b388
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-07-23 22:08:30 -0300

    Cor das bolinhas feito

--- Macro01/Macro01/View/Componentes/ScoreView.swift
+++ Macro01/Macro01/View/Componentes/ScoreView.swift
@@ -1,16 +1,7 @@
-////
-////  ScoreView.swift
-////  Macro01
-////
-////  Created by Higor  Lo Castro on 10/06/23.
-////
-
 import SwiftUI
 
 struct ScoreView: View {
     let maxDots = 5 // Bolinhas Total
-    let activeColorPlayOne = Color(hex: "88BD8E")
-    let activeColorPlayTwo = Color(hex: "C27C7C")
     let inactiveColor = Color(hex: "FFF2D9") // Cor da bolinha inativa
     let centerDotSize: CGFloat = 30 // Tamanho da bolinha do centro
     
@@ -22,25 +13,23 @@ struct ScoreView: View {
             // Bolinhas do lado esquerdo
             ForEach(0..<2) { index in
                 Circle()
-                    .fill(index < player1Wins ? activeColorPlayOne : inactiveColor)
+                    .fill(index < player1Wins ? Color(hex: "88BD8E") : inactiveColor)
                     .frame(width: 20, height: 20)
             }
             
             // Bolinha do centro
             Circle()
-                .fill((player1Wins >= 3 || player2Wins >= 3) ? activeColorPlayOne : inactiveColor)
+//                .fill((player1Wins >= 3 || player2Wins >= 3) ? Color(hex: "88BD8E") : inactiveColor)
+            
+                .fill(player1Wins >= 3 ? Color(hex: "88BD8E") : (player2Wins >= 3 ? Color(hex: "C27C7C") : inactiveColor))
                 .frame(width: centerDotSize, height: centerDotSize)
             
             // Bolinhas do lado direito
             ForEach(0..<2) { index in
                 Circle()
-                    .fill((index == 0 && player2Wins >= 2) || (index == 1 && player2Wins >= 1) ? activeColorPlayTwo : inactiveColor)
+                    .fill((index == 0 && player2Wins >= 2) || (index == 1 && player2Wins >= 1) ? Color(hex: "C27C7C") : inactiveColor)
                     .frame(width: 20, height: 20)
             }
         }
     }
 }
-
-
-
-
