830d57acf19e247ff873ace40205dba25f478687
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-08-04 09:26:19 -0300

    Corrigir Logo

--- Macro01/Macro01/Macro01App.swift
+++ Macro01/Macro01/Macro01App.swift
@@ -2,16 +2,7 @@ import SwiftUI
 
 @main
 struct Macro01App: App {
-    
-    init() {
-        for family in UIFont.familyNames {
-            print("family")
-            for font in UIFont.fontNames(forFamilyName: family) {
-                print("     \(font)")
-            }
-        }
-    }
-    
+        
     var body: some Scene {
         WindowGroup {
             SplashScreen()
