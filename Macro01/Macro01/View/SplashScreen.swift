7e84bca747294a942a2f47583d23a9c217b15b44
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-08-04 08:55:29 -0300

    Corrigir Tamanho do Logo

--- Macro01/Macro01/View/SplashScreen.swift
+++ Macro01/Macro01/View/SplashScreen.swift
@@ -18,52 +18,53 @@ struct SplashScreen: View {
     let screenHeight = UIScreen.main.bounds.size.height
     
     var body: some View {
-        VStack {
-            if showHomeView {
-                MainView()
-            } else {
-                VStack(){
-                    Image("splash")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 250, height: 250)
-
-                }
-                .scaleEffect(scale)
-                .opacity(opacity)
-                .onAppear{
-                    withAnimation(
-                        .easeIn(duration: 2)){
-                            self.scale = 1.0
-                            self.opacity = 1.0
+        ZStack {
+            Image("backgroundGeral")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                if showHomeView {
+                    MainView()
+                } else {
+                    VStack(){
+                        Image("logoSplash")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 250, height: 250)
+                        
+                    }
+                    .scaleEffect(scale)
+                    .opacity(opacity)
+                    .onAppear{
+                        withAnimation(
+                            .easeIn(duration: 2)){
+                                self.scale = 1.0
+                                self.opacity = 1.0
+                            }
+                        withAnimation(Animation.linear.delay(2)) {
+                            showHomeView = true
                         }
-                    withAnimation(Animation.linear.delay(2)) {
-                        showHomeView = true
                     }
                 }
             }
         }
-        .background{
-            Rectangle()
-                .ignoresSafeArea()
-                .frame(width: screenWidth, height: screenHeight, alignment: .center)
-                .foregroundColor(Color("corFundoSplash"))
-        }
     }
 }
 
-extension UINavigationController: UIGestureRecognizerDelegate {
-    override open func viewDidLoad() {
-        super.viewDidLoad()
-        interactivePopGestureRecognizer?.delegate = self
-    }
 
-    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return viewControllers.count > 1
-    }
-
-    
-    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        true
-    }
-}
+//extension UINavigationController: UIGestureRecognizerDelegate {
+//    override open func viewDidLoad() {
+//        super.viewDidLoad()
+//        interactivePopGestureRecognizer?.delegate = self
+//    }
+//
+//    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+//        return viewControllers.count > 1
+//    }
+//
+//
+//    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+//        true
+//    }
+//}
