55ba1d4fd9a57e949e1f3f8cedd56272961357cf
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-08-06 16:18:52 -0300

    Tres cores na barra

--- Macro01/Macro01/View/Componentes/CountdownBar.swift
+++ Macro01/Macro01/View/Componentes/CountdownBar.swift
@@ -19,7 +19,22 @@ struct CountdownBar : View {
 
             RoundedRectangle(cornerRadius: 10)
                 .frame(width: 15, height: 230 * (1 - countdownFraction))
-                .foregroundColor(Color(hex: "#12e05e"))
+                .foregroundColor(getBarColor())
+        }
+    }
+    
+    func getBarColor() -> Color {
+        let greenColor = Color(hex: "#15e012")
+        let yellowColor = Color(hex: "#d6e012")
+        let redColor = Color(hex: "#e01212")
+        
+        if countdownFraction >= 0.67 {
+            return redColor
+        } else if countdownFraction >= 0.33 {
+            return yellowColor
+        } else {
+            return greenColor
         }
     }
 }
+
